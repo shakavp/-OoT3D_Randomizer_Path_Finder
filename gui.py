7d0c9e8d4cd54de00b8868052e239fc5161614da
Geraldo Castro Zampoli <shaka_vp@yahoo.com.br>
2022-01-22 02:41:48 -0300

    Including gui draft

---
+++ gui.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import ttk
+
+class MyApp():
+    def __init__(self):
+        self.root = tk.Tk()
+
+        choices = [
+            "House", 
+            "Market",
+            "Kokori Florest",
+            "Lost Woods"
+            ]
+
+        self.root.title('Path Calculator')
+
+        label1 = tk.Label(self.root, text = "Origin")
+        label1.grid(column=0, row=0, padx=(10, 10))
+
+        self.combo1 = ttk.Combobox(self.root, values=choices)
+        self.combo1.grid(column=0, row=1, padx=(10, 10))
+        self.combo1.current(0)
+
+        label2 = tk.Label(self.root, text = "Destination")
+        label2.grid(column=2, row=0, padx=(10, 10))
+
+        self.combo2 = ttk.Combobox(self.root, values=choices)
+        self.combo2.grid(column=2, row=1, padx=(10, 10))
+        self.combo2.current(0)
+
+        button1 = ttk.Button(self.root, text="Add path", command=self.add)
+        button1.grid(column=1, row=3, pady=(10, 10))
+
+        button2 = ttk.Button(self.root, text="Calculate shortest path", command=self.calculate_path)
+        button2.grid(column=1, row=4, pady=(0, 30))
+
+        self.text_box = tk.Text(self.root, font= ('Arial', 8), height=22, width=70)
+        self.text_box.grid(column=0, row=5, columnspan=3, pady=(0, 10))
+        self.text_box.config(state='disabled')
+
+    def update_text(self, msg):
+        self.text_box.config(state='normal')
+        self.text_box.delete(1.0, 'end')
+        self.text_box.insert('end', msg)
+        self.text_box.config(state='disabled')
+
+    def add(self):
+        origin_add = self.combo1.get()
+        destination_add = self.combo2.get()
+        msg_aux = f"Adding {origin_add} -> {destination_add} to the digraph\n"
+        self.update_text(msg_aux)
+
+    def calculate_path(self):
+        origin_add = self.combo1.get()
+        destination_add = self.combo2.get()
+        msg_aux = f"Searching shortest path from {origin_add} to {destination_add}\n"
+        self.update_text(msg_aux)
+
+app = MyApp()
+app.root.mainloop()
